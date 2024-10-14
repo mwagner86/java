6a9188f84ff3c25319e410ff615a96b12ce74435
Maximilian Wagner <maximilian.wagner@th-brandenburg.de>
2024-10-14 21:24:26 +0200

    upload test from windows

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld
 		System.out.println("Hello World");
 		System.out.println("Hello World\n");
 		System.out.print("Hello World");
+		System.out.print("Something different entirely");
 	}
 }
