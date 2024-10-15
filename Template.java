9d373e8d5268c77c92f0743e5a142c7ab42615a0
Maximilian Wagner <mwagner@students.42wolfsburg.de>
2024-10-15 18:53:05 +0200

    template.java

---
+++ Template.java
@@ -0,0 +1,22 @@
+/**
+ * Dateiname: Template.java 
+ * Beschreibung: Template zum Erstellen von Java Aufgaben.
+ *
+ * @author Maximilian Wagner (wagnerm@th-brandenburg.de)
+ * @version 1.00, 10/2024
+*/
+
+public class Template
+{
+	/**
+	* Soll die Klasse von der Eingabekonsole aus aufgerufen werden koennen, dann
+	* muss die hier aufgefuehrte main-Methode enthalten sein. Diese Methode ist der
+	* definierte Einstiegspunkt fuer die Ausfuehrung.
+	*
+	* @param args Kommandozeilenparameter f&uuml;r main Methode
+	*/
+	public static void main(String[] args)
+	{
+		// Here goes nothing
+	}
+}
